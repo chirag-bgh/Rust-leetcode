9f249bcaaa56d7ed450c6b237101287f26233d28
chirag-bgh <chirag2bsingh@gmail.com>
2022-12-11 23:47:15 +0530

    add #1

---
+++ src/#1.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+pub struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::with_capacity(nums.len());
+        for (index, num) in nums.iter().enumerate() {
+            match map.get(&(target - num)) {
+                None => {
+                    map.insert(num, index);
+                }
+                Some(sub_index) => return vec![*sub_index as i32, index as i32],
+            }
+        }
+        vec![]
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+}
